e443ddf495335ca3341c6ef1d44c46676f39e8a4
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2024-09-21 15:58:19 +0300

    MenuListItems finished

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -1,21 +1,58 @@
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
+import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import { useNavigate } from "react-router-dom";
+
+const icons = [
+  {
+    icon: <SpaceDashboardIcon />,
+    title: "Dashboard",
+    url: "/stock/",
+  },
+  {
+    title: "Purchases",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+];
 
 const MenuListItems = () => {
+  const navigate = useNavigate();
   return (
     <List>
-      {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-        <ListItem key={text} disablePadding>
-          <ListItemButton>
-            <ListItemIcon sx={{ color: "white" }}>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText sx={{ color: "white" }} primary={text} />
+      {icons.map((item, index) => (
+        <ListItem key={index} disablePadding>
+          <ListItemButton onClick={() => navigate(item.url)}>
+            <ListItemIcon sx={{ color: "white" }}>{item.icon}</ListItemIcon>
+            <ListItemText sx={{ color: "white" }} primary={item.title} />
           </ListItemButton>
         </ListItem>
       ))}
