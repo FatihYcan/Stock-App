286621f8ce7412e903621c61b9b8fcf218f8934f
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2024-09-24 17:59:45 +0300

    Products and ProductModal starter

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -29,7 +29,6 @@ const Firms = () => {
 
   useEffect(() => {
     getStocks("firms");
-    getStocks("sales");
   }, []);
 
   // console.log(firms);
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,55 @@
+import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import useStockCalls from "../service/useStockCalls";
+import { useSelector } from "react-redux";
+import { Container } from "react-bootstrap";
+// import ProductCard from "../components/ProductCard";
+import ProductModal from "../components/ProductModal";
+
 const Products = () => {
-  return <div>Products</div>;
+  const { getStocks } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setInfo({
+      name: "",
+      phone: "",
+      address: "",
+      image: "",
+    });
+  };
+
+  useEffect(() => {
+    getStocks("firms");
+  }, []);
+
+  // console.log(firms);
+
+  return (
+    <Container fluid>
+      <Typography variant="h5" color="red" mb={2}>
+        Products
+      </Typography>
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+    </Container>
+  );
 };
 
 export default Products;
