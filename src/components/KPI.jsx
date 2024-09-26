c73b8c7f9b2a8e4fe95b9a0c4e0b2e56c6d82e3d
Fatih Yakutcan <f.yakutcan.32@gmail.com>
2024-09-26 17:45:12 +0300

    KPI finished

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -2,41 +2,64 @@ import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
 import { amber, deepPurple, pink } from "@mui/material/colors";
-import { Avatar, Stack } from "@mui/material";
+import { Avatar, Box, Stack, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper";
-
-const kpiData = [
-  {
-    id: 1,
-    title: "Sales",
-    amount: "$16900",
-    icon: <MonetizationOnIcon sx={{ fontSize: "2.5rem" }} />,
-    bgColor: deepPurple[100],
-    color: deepPurple[700],
-  },
-  {
-    id: 2,
-    title: "Sales",
-    amount: "$16900",
-    icon: <ShoppingCartIcon sx={{ fontSize: "2.5rem" }} />,
-    bgColor: pink[100],
-    color: pink[700],
-  },
-  {
-    id: 3,
-    title: "Sales",
-    amount: "$16900",
-    icon: <PaymentsIcon sx={{ fontSize: "2.5rem" }} />,
-    bgColor: amber[100],
-    color: amber[700],
-  },
-];
+import { useSelector } from "react-redux";
 
 const KPI = () => {
+  const { sales, purchases } = useSelector((state) => state.stock);
+
+  const totalSales = sales?.reduce((acc, val) => acc + val.amount, 0);
+  const totalPurchases = purchases?.reduce((acc, val) => acc + val.amount, 0);
+
+  const kpiData = [
+    {
+      id: 1,
+      title: "Sales",
+      amount: totalSales.toFixed(2),
+      icon: <MonetizationOnIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: deepPurple[100],
+      color: deepPurple[700],
+    },
+    {
+      id: 2,
+      title: "Profit",
+      amount: (totalSales - totalPurchases).toFixed(2),
+      icon: <ShoppingCartIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: pink[100],
+      color: pink[700],
+    },
+    {
+      id: 3,
+      title: "Pruchases",
+      amount: totalPurchases.toFixed(2),
+      icon: <PaymentsIcon sx={{ fontSize: "2rem" }} />,
+      bgColor: amber[100],
+      color: amber[700],
+    },
+  ];
+
   return (
-    <Stack justifyContent="center" alignItems="center">
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      flexWrap="wrap"
+      direction="row"
+      gap="1rem"
+      marginBottom="2rem"
+    >
       {kpiData.map((item) => (
-        <Paper elevation={5} key={item.id}>
+        <Paper
+          elevation={5}
+          key={item.id}
+          sx={{
+            padding: "1rem",
+            display: "flex",
+            gap: "1rem",
+            width: "250px",
+            alignItems: "center",
+          }}
+        >
           <Avatar
             sx={{
               bgcolor: item.bgColor,
@@ -47,6 +70,11 @@ const KPI = () => {
           >
             {item.icon}
           </Avatar>
+
+          <Box>
+            <Typography variant="button"> {item.title} </Typography>
+            <Typography variant="h5"> ${item.amount} </Typography>
+          </Box>
         </Paper>
       ))}
     </Stack>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import { Container } from "react-bootstrap";
 import KPI from "../components/KPI";
 import Charts from "../components/Charts";
+import { useEffect } from "react";
+import useStockCalls from "../service/useStockCalls";
 
 const Home = () => {
+  const { getStocks } = useStockCalls();
+
+  useEffect(() => {
+    getStocks("sales");
+    getStocks("purchases");
+  }, []);
+
   return (
     <Container fluid>
       <KPI />
